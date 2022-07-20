fc92b864cfd00568ece27b85a73172469c4a81ac
Lauri <laurianarodrigueslima@gmail.com>
2022-07-20 19:33:44 -0300

    seconds

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ const buttonSet = document.querySelector('.set')
 const buttonStop = document.querySelector('.stop')
 const buttonSoundOn = document.querySelector('.soundOn')
 const buttonSoundOff = document.querySelector('.soundOff')
+let minutes;
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
+
+function countdown(){
+  setTimeout(function() {
+    let seconds =  Number(secondsDisplay.textContent)
+    if( seconds <= 0 ) {
+      seconds = 60
+    }
+    secondsDisplay.textContent = seconds - 1
+    countdown()
+  }, 1000)
+}
 
 buttonPlay.addEventListener('click', function() {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonSet.classList.add('hide')
   buttonStop.classList.remove('hide')
+
+  countdown()
+
 })
 
 buttonPause.addEventListener('click', function() {
@@ -34,4 +51,8 @@ buttonSoundOff.addEventListener('click', function() {
   buttonSoundOn.classList.remove('hide')
 })
 
+buttonSet.addEventListener('click', function() {
+  minutes = prompt('Quantos minutos?')
+  minutesDisplay.textContent = minutes
+})
 
